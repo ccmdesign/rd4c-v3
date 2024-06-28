ec62ecea7619339afb29640621fa32791cca6171
Claudio <claudio@ccmdesign.ca>
2024-06-28 12:19:05 -0700

    navigation mobile

---
+++ components/rd-menu.vue
@@ -0,0 +1,86 @@
+<template>
+  <nav class="menu" :class="{ 'menu--open': isMenuOpen }">
+    <rd-button @click="toggleMenu" class="menu__toggle" icon-before="menu" visual="ghost" color="white" size="full-width" /> 
+    <nuxt-link v-for="item in menuItems" :key="item.name" class="menu__item" :to="item.link" :target="item.target">{{
+      item.name }}</nuxt-link>
+
+  </nav>
+</template>
+
+<script setup>
+import { useNavigation } from '~/pages/composables/useNavigation';
+
+const { menuItems } = useNavigation();
+
+const isMenuOpen = ref(false);
+
+function toggleMenu() {
+  isMenuOpen.value = !isMenuOpen.value;
+}
+
+// Optionally, you can add a watcher to close the menu when the route changes
+watch(() => useRoute().path, () => {
+  isMenuOpen.value = false;
+});
+
+
+
+
+onMounted(() => {
+  nextTick(() => {
+    const menuElement = document.querySelector('.menu');
+    const height = menuElement ? menuElement.scrollHeight : 0;
+    document.documentElement.style.setProperty('--menu-height', `${height}px`);
+  });
+});
+</script>
+
+<style lang="scss" scoped>
+.menu {
+  @media screen and (max-width: 768px) {
+    --menu-height: 330px; // magic number calculated based on the number of menu items in useNavigation
+    transition: all .3s ease-in-out;
+    position: fixed;
+    display: block;
+    z-index: 1000;
+    width: 100%;
+    height: auto;
+    bottom: calc(-1 * var(--menu-height));
+    overflow: hidden;
+    left: 0;
+    background: var(--base-color);
+    margin: 0;
+    text-align: center;
+
+    .menu__item {
+      display: block;
+      padding: var(--s-1) 0;
+      color: var(--white-color);
+      text-decoration: none;
+      font-weight: 500;
+      border-bottom: 1px solid hsla(var(--white-hsl), .2);
+      
+    }
+
+    &.menu--open { bottom: 0; }
+  }
+
+  @media screen and (min-width: 768px) {
+    display: flex;
+    justify-content: space-between;
+    list-style: none;
+    gap: var(--s1);
+  }
+}
+
+.menu__toggle {
+  @media screen and (min-width: 768px) { display: none; }
+
+  @media screen and (max-width: 768px) {
+    font-size: 1.25rem;
+    display: block;
+    cursor: pointer;
+    padding-block: var(--s-2) !important;
+  }
+}
+</style>
--- components/rd-top-bar.vue
+++ components/rd-top-bar.vue
@@ -2,21 +2,12 @@
   <center-l measure="var(--wide)">
     <header class="top-bar">
       <nuxt-link to="/"><img src="/images/rd4c-logo.svg" alt="Homepage"></nuxt-link>
-      <nav>
-        <ul class="menu">
-          <li v-for="item in menuItems" :key="item.name">
-            <nuxt-link :to="item.link" :target="item.target">{{ item.name }}</nuxt-link>
-          </li>
-        </ul>
-      </nav>
+      <rd-menu />
     </header>
   </center-l>
 </template>
 
 <script setup>
-import { useNavigation } from '~/pages/composables/useNavigation';
-
-const { menuItems } = useNavigation();
 
 </script>
 
@@ -26,26 +17,4 @@ const { menuItems } = useNavigation();
   justify-content: space-between;
   align-items: center;
 }
-
-.menu {
-  @media screen and (max-width: 768px) {
-    position: fixed;
-    display: block;
-    z-index: 1000;
-    width: 100%;
-    height: auto;
-    bottom: 0;
-    overflow: hidden;
-    left: 0;
-    background: red;
-    margin: 0;
-    text-align: center;
-  }
-
-  @media screen and (min-width: 768px) {
-    display: flex;
-    justify-content: space-between;
-    list-style: none;
-    gap: var(--s1);
-  }
-}</style>
+</style>

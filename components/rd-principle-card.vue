59614333db8d229f225a2d003dbb3d69d078b0c3
Claudio <claudio@ccmdesign.ca>
2024-07-15 11:06:40 -0700

    principle card

--- components/rd-principle-card.vue
+++ components/rd-principle-card.vue
@@ -1,31 +1,15 @@
 <template>
-  <div class="rd-principle-card">
-    <stack-l>
-      <h3>{{ content.name }}</h3>
-      <p>{{ content.tagline }}</p>
-    </stack-l>
+  <div>
+
   </div>
 </template>
 
 <script setup>
-defineProps({
-  content: {
-    type: Object,
-    required: true
-  }
-})
 
 </script>
 
 <style lang="scss" scoped>
-.rd-principle-card {
-  background-color: var(--base-color);
-  padding: var(--s1);
-  * { color: white !important;}
 
-  @media screen and (max-width: 768px) {
-    height: 70svh;
-    box-sizing: border-box;
-  }
-}
 </style>
+
+

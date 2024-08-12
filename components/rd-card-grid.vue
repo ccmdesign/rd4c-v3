af5bc8686932631df67d7b693eb11be8a1e27313
Claudio <claudio@ccmdesign.ca>
2024-08-12 07:29:53 -0700

    card grid and card

---
+++ components/rd-card-grid.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="rd-card-grid">
+    <slot></slot>
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+.rd-card-grid {
+  --_padding-inline: 1rem;
+  --_content-max-width: 960px;
+}
+
+.rd-card-grid {
+  display: grid;
+  
+  padding-inline: var(--_padding-inline);
+  grid-template-columns: repeat(1, 1fr);
+  gap: var(--s1);
+  
+  @media screen and (min-width: 768px) {
+    grid-template-columns: repeat(auto-fit, minmax(min(400px, 100%), 1fr));
+  }
+}
+</style>
---
+++ components/rd-card.vue
@@ -0,0 +1,119 @@
+<template>
+  <div class="rd-card" :color="content.color">
+    <h5>{{ content.brow }}</h5>
+    <h3>{{ content.heading }}</h3>
+    <h4>{{ content.tagline }}</h4>
+    <p>{{ formatDate(content.date) }}</p>
+    
+<!-- @Todo: Douglas, não sei exatamente como devemos puxar as imagens. Achei esse
+     filename_disk no Directus/Content. Por favor confira se isso vai funcionar.
+     Se não, me avise como devemos fazer. -->
+    <!-- <img v-if="content.cover_image.filename_disk" :src="`https://cms.thegovlab.com/assets/${content.cover_image.filename_disk}`" alt=""> -->
+    <!-- <img src="/images/default.jpg" alt=""> -->
+    <div class="rd-card__action">
+      <nuxt-link v-if="content.url" class="button" color="primary" :to="content.url">Read
+        More</nuxt-link>
+      <nuxt-link v-else class="button" color="primary" :to="content._path">
+        Read More</nuxt-link>
+    </div>
+    <pre>{{ content }}</pre>
+  </div>
+</template>
+
+<script setup>
+import { formatDate } from '~/composables/useFilters';
+
+const props = defineProps({
+  content: {
+    type: Object,
+    required: true,
+    default: () => ({})
+  }
+});
+
+// const slugifiedHeading = computed(() => props.content.slug);
+
+// @ToDo: Douglas, estávamos usando isso na versão anterior. Atualemente estou usando o
+// attr _path que vem do Directus/Content. Por favor, cheque se a minha solução funciona ou 
+// devemos continuar usando o slug.
+const computedUrl = computed(() => {
+  return `/articles/${props.content.slug}`;
+});
+
+</script>
+
+<style lang="scss" scoped>
+.rd-card {
+  --_gap: var(--s-2);
+  --_padding: var(--s0);
+}
+
+
+.rd-card {
+  display: grid;
+  grid-template-rows: auto auto auto auto auto 1fr;
+  grid-template-columns: 1fr;
+  gap: var(--_gap);
+  grid-template-areas: 
+    "image"
+    "brow"
+    "heading"
+    "tagline"
+    "date"
+    "action";
+
+  border: 1px solid hsla(var(--base-hsl), .1);
+  padding-bottom: var(--_padding);
+  border-radius: var(--base-border-radius);  
+  overflow: clip;
+
+  > *:not(img) {
+    padding-inline: var(--_padding);
+  }
+  
+  img {
+    width: 100%;
+    object-fit: cover;
+    aspect-ratio: 16 / 9;
+    grid-area: image;
+    margin-bottom: var(--_padding);
+  }
+
+  h5 { 
+    grid-area: brow;
+    font-size: .85rem;
+    text-transform: uppercase;
+    font-weight: bold;
+    color: var(--accent-color);
+  }
+
+  h3 {
+    grid-area: heading;
+    font-size: 1.15rem;
+    line-height: 1.45;
+  }
+  
+  h4 {
+    grid-area: tagline;
+    font-size: 1rem;
+    line-height: 1.45;
+  }
+
+  p {
+    grid-area: date;
+    font-size: .85rem;
+    font-weight: 600;
+    color: var(--primary-color);
+  }
+
+  .rd-card__action {
+    margin-top: var(--_padding);
+    grid-area: action;
+    display: flex;
+    // align-items: flex-end;
+    > * {
+      margin-top: auto;
+    }
+  }
+}
+</style>

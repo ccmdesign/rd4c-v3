1151471c73acee9db9239e3a9339c88be27b520d
Claudio <claudio@ccmdesign.ca>
2024-08-13 09:04:19 -0700

    principles cards - WIP

--- components/rd-principle-card.vue
+++ components/rd-principle-card.vue
@@ -1,8 +1,12 @@
 <template>
-  <div class="principle-card">
-    <h3>{{content.name}}</h3>
-    <h4>{{content.tagline}}</h4>
-    <p>{{content.description}}</p>
+  <div class="principle-card" :principle="content.slug" :n="n" :expanded="expanded" @click="expanded = !expanded">
+    <div class="content">
+      <div class="number">{{ principle_n }}</div>
+      <h3>{{content.name}}</h3>
+      <!-- <h4>{{content.tagline}}</h4> -->
+      <!-- <p>{{content.description}}</p> -->
+    </div>
+    
   </div>
 </template>
 
@@ -15,19 +19,117 @@ const props = defineProps({
       brow: '',
       heading: '',
       description: '',
-      color: '',
+      slug: '',
       url: ''
     })
+  },
+  n: {
+    type: Number,
+    required: true,
+    default: 0
+  },
+  expanded: {
+    type: Boolean,
+    default: false
   }
 });
+
+const principle_n = computed(() => {
+  return props.n.toString().padStart(2, '0');
+});
+
+
 
 </script>
 
 <style lang="scss" scoped>
 .principle-card {
-  border: 1px solid #000;
-  padding: 20px;
-  margin: 10px;
+  padding: var(--s1);
+  // aspect-ratio: 1/1;
+  place-content: center;
+  gap: var(--s0);
+  border-radius: var(--base-border-radius);
+  
+  // clip-path: path("M9,297.7l98.9-98.9,98.9-98.9c4.8-4.8,12.5-4.8,17.2,0l98.9,98.9,98.9,98.9c4.8,4.8,4.8,12.5,0,17.2l-98.9,98.9-98.9,98.9c-4.8,4.8-12.5,4.8-17.2,0l-98.9-98.9L9,314.9c-4.8-4.8-4.8-12.5,0-17.2Z");
+}
+
+.principle-card[expanded="true"] {
+  // transform: scale(1.05);
+  transition: transform 0.3s;
+  aspect-ratio: 3/2;
+  grid-column: span 3;
+  grid-row: span 2;
+}
+
+.principle-card .content { text-align: center; }
+
+.principle-card {
+  --_principle-hsl: var(--rd-purple);
+  background-color: hsla(var(--_principle-color), 1);
+  position: relative;
+}
+
+.principle-card[data-n="1"] {
+  --_principle-color: var(--rd-blue);
+}
+
+.principle-card[data-n="2"] {
+  --_principle-color: var(--rd-green);
+}
+
+.principle-card[data-n="3"] {
+  --_principle-color: var(--rd-orange);
+}
+
+.principle-card[data-n="4"] {
+  --_principle-color: var(--rd-purple);
+}
+
+.principle-card[data-n="5"] {
+  --_principle-color: var(--rd-red);
+}
+
+.principle-card[data-n="6"] {
+  --_principle-color: var(--rd-yellow);
+}
+
+.principle-card[data-n="7"] {
+  --_principle-color: var(--rd-teal);
+}
+
+.principle-card[data-n="8"] {
+  --_principle-color: var(--base-hsl);
+}
+
+.principle-card .number {
+  font-size: clamp(7rem, 13vh, 15rem);
+  // @media screen and (min-width: 768px) {font-size: 15rem;}
+  font-weight: 800;
+  color: var(--white-color-10);
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.principle-card h3 {
+  font-size: clamp(1rem, 2vh, 1.75rem);
+  font-weight: bold;
+  color: var(--white-color);
+  // text-align: center;
+}
+
+.principle-card h4 {
+  color: var(--white-color);
+  font-weight: 300;
+}
+
+.principle-card a {
+  color: var(--accent-color);
 }
 </style>
 
--- pages/principles.vue
+++ pages/principles.vue
@@ -10,20 +10,17 @@
         outcomes for children. The challenge is to understand the potential risks and ensure that the collection,
         analysis and use of data on children does not undermine these benefits.</p>
       <p>Drawing upon field-based research and established good practice, RD4C aims to highlight and support best
-        practice in our work; identify challenges and develop practical tools to assist practitioners in evaluating and
-        addressing them; and encourage a broader discussion on actionable principles, insights, and approaches for
+        practice in our work; identify challenges and develop practical tools to assist practitioners in evaluating
+        and addressing them; and encourage a broader discussion on actionable principles, insights, and approaches for
         responsible data management.</p>
-      <p><base-button visual="primary">Download the Report</base-button></p>
+      <p><rd-button color="primary">Download the Report</rd-button></p>
     </rd-base-section>
 
     <rd-base-section>
       <h2>Principles</h2>
-      <!-- <reel-l> -->
-      <stack-l>
-        <rd-principle-card v-for="i in principles" :content="i" />
-      </stack-l>
-
-      <!-- </reel-l> -->
+      <div class="principles-grid">
+        <rd-principle-card class="principles-grid__item" v-for="(i, index) in principles" :content="i" :data-n="index + 1" :n="index+1"/>
+      </div>
     </rd-base-section>
   </div>
   <!-- <a href="https://docs.google.com/document/d/1GWAdhbJy4Y89TYvRL2eycuprJaDbW-yX8EuzjnObt2M/edit">Google Doc</a> -->
@@ -42,5 +39,17 @@ const principles = await queryContent('principles').find();
 </script>
 
 <style scoped lang="scss">
+.principles-grid {
+  display: grid;
+  gap: var(--s-2);
+  grid-template-columns: 1fr;
+  grid-row: auto;
+}
+
+.principles-grid__item {
+  aspect-ratio: 4/1;
+}
+
+
 
 </style>

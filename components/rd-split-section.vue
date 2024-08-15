1721eed51ea82b774a5515791957158c6d20e86a
Claudio <claudio@ccmdesign.ca>
2024-08-15 07:25:24 -0700

    adaptation to cms-content - split-section

--- components/rd-split-section.vue
+++ components/rd-split-section.vue
@@ -1,12 +1,12 @@
 <template>
   <section class="rd-split-section" :image="image">
-    <img v-if="image" :src="image" alt="">
-    
-    <div class="extra-content" v-else >
-      <slot name="extra"></slot>
+    <div class="panel panel--image">
+      <slot name="extra">
+        <img :src="image" alt="">
+      </slot>
     </div>
 
-    <div class="content">
+    <div class="panel panel--content">
       <slot></slot>
     </div>
   </section>
@@ -28,68 +28,87 @@ defineProps({
 
 <style lang="scss" scoped>
 .rd-split-section {
-  --_gap: var(--s-1);
   --_padding-inline: var(--s0);
   --_padding-block: var(--s2);
+  --_gap: var(--s1);
 
   @media screen and (min-width: 768px) {
-    --_gap: var(--s0);
     --_padding-inline: var(--s3);
     --_padding-block: var(--s3);
+    --_gap: var(--s0);
   }
 }
 
 .rd-split-section { 
-  display: flex;
-  flex-direction: column;
+  display: grid;
+  grid-template-columns: 1fr;
+  grid-template-rows: 1fr max(auto, 1fr);
+  grid-template-areas: "panel1" "panel2";
 
   @media screen and (min-width: 768px) {
-    flex-direction: row;
-
-    &:nth-of-type(2n) { 
-      flex-direction: row-reverse; 
-      .content { text-align: right; }
-    }
+    grid-template-columns: 1fr 1fr;
+    grid-template-rows: 1fr;
+    grid-template-areas: "panel1 panel2";
   }
 }
 
-.rd-split-section > * { flex: 1; }
+.panel {
+  max-width: 100svw; // Probably there is a way to remove this. 
+  @media screen and (min-width: 768px) {
+    max-width: 50svw; // Probably there is a way to remove this. 
+  }
+}
 
-.rd-split-section > img {
-  @media screen and (min-width: 768px) { width: 50%; }
+.panel--image { grid-area: panel1; }
+.panel--content { grid-area: panel2; }
 
-  & {
-    flex: 1;
+@media screen and (min-width: 768px) {
+  .rd-split-section:nth-of-type(odd) {
+    grid-template-areas: "panel2 panel1";
+    .panel--content { text-align: right; }
+  }
+}
+
+.panel--image {
+  aspect-ratio: 1 / 1;
+  background-color: lightblue;
+  object-position: center;
+
+  img {
     object-fit: cover;
-    object-position: center;
-    aspect-ratio: 1 / 1;
+    height: 100%;
+    
   }
 }
 
-.rd-split-section .content {
+.panel--content {
   display: flex;
   flex-direction: column;
-  justify-content: center;
   gap: var(--_gap);
+  justify-content: center;
   padding: var(--_padding-block) var(--_padding-inline);
-  text-wrap: balance;
-  @media screen and (min-width: 768px) { font-size: 125%; }
 }
 
-.rd-split-section :deep(h2) {
-  color: var(--primary-color);
-  // font-size: 2rem;
-}
-.rd-split-section :deep(h3) {
-  color: var(--primary-color);
-  // font-size: 1.5rem;
-}
-.rd-split-section :deep(h4) {
-  // font-size: 1rem;
-  font-weight: 900;
-  color: var(--primary-color);
-}
-.rd-split-section :deep(p) {
-  color: var(--base-color);
-}
+// .rd-split-section .content {
+//   display: flex;
+//   flex-direction: column;
+//   justify-content: center;
+//   gap: var(--_gap);
+//   padding: var(--_padding-block) 0;
+//   text-wrap: balance;
+// }
+
+// .rd-split-section :deep(h2) {
+//   color: var(--primary-color);
+// }
+// .rd-split-section :deep(h3) {
+//   color: var(--primary-color);
+// }
+// .rd-split-section :deep(h4) {
+//   font-weight: 900;
+//   color: var(--primary-color);
+// }
+// .rd-split-section :deep(p) {
+//   color: var(--base-color);
+// }
 </style>

63c7a83217ccad489d3502c94765b50afdb3c59f
Claudio <claudio@ccmdesign.ca>
2024-08-20 14:24:45 -0700

    split section with Andy Bell's responsive grid

--- components/rd-split-section.vue
+++ components/rd-split-section.vue
@@ -1,5 +1,5 @@
 <template>
-  <section class="rd-split-section" :image="image">
+  <section class="rd-split-section auto-grid" :image="image" data-layout="50-50">
     <div class="panel panel--image">
       <slot name="extra">
         <img :src="image" alt="">
@@ -39,35 +39,42 @@ defineProps({
   }
 }
 
-.rd-split-section { 
-  display: grid;
-  grid-template-columns: 1fr;
-  grid-template-rows: 1fr max(auto, 1fr);
-  grid-template-areas: "panel1" "panel2";
-
-  @media screen and (min-width: 768px) {
-    grid-template-columns: 1fr 1fr;
-    grid-template-rows: 1fr;
-    grid-template-areas: "panel1 panel2";
-  }
-}
-
-.panel {
-  max-width: 100svw; // Probably there is a way to remove this. 
-  @media screen and (min-width: 768px) {
-    max-width: 50svw; // Probably there is a way to remove this. 
-  }
-}
-
-.panel--image { grid-area: panel1; }
-.panel--content { grid-area: panel2; }
-
 @media screen and (min-width: 768px) {
   .rd-split-section:nth-of-type(odd) {
-    grid-template-areas: "panel2 panel1";
+    .panel--image { order: 2; }
     .panel--content { text-align: right; }
   }
 }
+
+// .rd-split-section { 
+//   display: grid;
+//   grid-template-columns: 1fr;
+//   grid-template-rows: 1fr max(auto, 1fr);
+//   grid-template-areas: "panel1" "panel2";
+
+//   @media screen and (min-width: 768px) {
+//     grid-template-columns: 1fr 1fr;
+//     grid-template-rows: 1fr;
+//     grid-template-areas: "panel1 panel2";
+//   }
+// }
+
+// .panel {
+  // max-width: 100svw; // Probably there is a way to remove this. 
+  // @media screen and (min-width: 768px) {
+  //   max-width: 50svw; // Probably there is a way to remove this. 
+  // }
+// }
+
+// .panel--image { grid-area: panel1; }
+// .panel--content { grid-area: panel2; }
+
+// @media screen and (min-width: 768px) {
+//   .rd-split-section:nth-of-type(odd) {
+//     grid-template-areas: "panel2 panel1";
+//     .panel--content { text-align: right; }
+//   }
+// }
 
 .panel--image {
   aspect-ratio: 1 / 1;
@@ -85,7 +92,4 @@ defineProps({
   gap: var(--_gap);
   justify-content: center;
   padding: var(--_padding-block) var(--_padding-inline);
-}
-
-
-</style>
+}</style>

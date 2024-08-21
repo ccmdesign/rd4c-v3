b300586126483e14623e166d135061531cde457d
Claudio <claudio@ccmdesign.ca>
2024-08-21 11:20:08 -0700

    principle card (large), less flashy

--- components/rd-principle-card.vue
+++ components/rd-principle-card.vue
@@ -52,14 +52,19 @@ const principle_n = computed(() => {
 
 // decorations
 .principle-card {
+  --_principle-padding: var(--space-m-l);
+
   box-sizing: border-box;
-  border-radius: var(--base-border-radius);
+  border-radius: 0 var(--base-border-radius) var(--base-border-radius) 0;
   transition: all .3s;
   box-shadow: 0 0 16px hsla(var(--base-hsl), .1);
+  border: 2px solid hsla(var(--_principle-hsl), 1);
+  border-left: 8px solid hsla(var(--_principle-hsl), 1);
 
   &:hover {
-    transform: scale(1.1);
+    transform: scale(1.05);
     box-shadow: 0 0 32px hsla(var(--base-hsl), .25);
+    --_principle-padding: var(--space-l-xl);
   }
 }
 
@@ -67,48 +72,46 @@ const principle_n = computed(() => {
   font-size: clamp(7rem, 13vh, 15rem);
   line-height: 1;
   font-weight: 800;
-  color: hsla(var(--white-hsl), .15);
+  color: hsla(var(--white-hsl), .5);
+  
+  -webkit-text-stroke: 1px hsla(var(--_principle-hsl), 1);
+  text-stroke: 1px hsla(var(--_principle-hsl), 1); /* Standard property for other browsers */
   grid-area: number;
-  padding: var(--s1);
-  @media screen and (min-width: 768px) { padding-inline: var(--s2); }
+  padding: var(--space-s-l);
+
+  @supports not (-webkit-text-stroke: 1px black) {
+    .principle-card .number {
+      color: hsla(var(--_principle-hsl), 1);
+    }
+  }
 }
 
 .principle-card .content {
   grid-area: content;
   align-self: center;
   display: flex;
-  gap: var(--s-1);
+  gap: var(--space-3xs-2xs);
   flex-direction: column;
-  padding: var(--s1);
-  
-  @media screen and (min-width: 768px) {
-    padding: var(--s2) var(--s4) var(--s2) 0;
-  }
-  
+  padding: var(--_principle-padding);
+  color: hsla(var(--_principle-hsl), 1);
 }
 
-.content > * { color: var(--white-color); }
+.content > * { color: inherit }
 
 .principle-card h3 {
-  font-size: clamp(1rem, 4vh, 2.5rem);
+  font-size: var(--step-3);
   font-weight: 200;
 }
 
 .principle-card h4 {
   font-weight: 400;
-  font-size: clamp(1rem, 2vh, 1.5rem);
-  
+  font-size: var(--step-0);
 }
-
-.principle-card a {
-  color: var(--accent-color);
-}
-
 
 // Defining Colors
 .principle-card {
   --_principle-hsl: var(--rd-purple);
-  background-color: hsla(var(--_principle-hsl), 1);
+  background-color: hsla(var(--_principle-hsl), .1);
 }
 
 .principle-card[data-n="1"] {
@@ -142,7 +145,6 @@ const principle_n = computed(() => {
 .principle-card[data-n="8"] {
   --_principle-hsl: var(--base-hsl);
 }
-
 
 </style>
 

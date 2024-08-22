823d8b77d7f80080bdeb37bddd9cd7b5900222dc
Douglas Cantao <dcp.pereira@gmail.com>
2024-08-22 16:54:46 -0700

    Setep signup section wht formspree

--- components/rd-signup-section.vue
+++ components/rd-signup-section.vue
@@ -1,21 +1,21 @@
 <template>
   <!-- @ToDo: Translate the whole section -->
   <rd-split-section class="rd-signup-section" data-layout="50-50" regular-order>
-    <ccm-form class="form" aria-labelledby="signup-form">
+    <ccm-form @submit.prevent="onSubmit" class="form" aria-labelledby="signup-form">
       <!-- <base-input placeholder="Name" aria-label="Name"/> -->
-      <base-input placeholder="Email" aria-label="Email"/>
-      
+      <base-input placeholder="Email" aria-label="Email" type="email" name="email" v-model="email" required />
+
       <div>
         <label class="rd-checkbox" for="videos-updates">
-          <input type="checkbox" checked name="videos-updates" id="videos-updates" aria-labelledby="videos-updates-label">
+          <input type="checkbox" checked name="videos-updates" id="videos-updates" aria-labelledby="videos-updates-label" v-model="updates">
           <span id="videos-updates-label">{{ $t('signup.options.updates') }}</span>
         </label>
         <label class="rd-checkbox" for="join-conversation">
-          <input type="checkbox" name="join-conversation" id="join-conversation" aria-labelledby="join-conversation-label">
+          <input type="checkbox" name="join-conversation" id="join-conversation" aria-labelledby="join-conversation-label" v-model="conversation">
           <span id="join-conversation-label">{{ $t('signup.options.conversation') }}</span>
         </label>
         <label class="rd-checkbox" for="join-alliance">
-          <input type="checkbox" name="join-alliance" id="join-alliance" aria-labelledby="join-alliance-label">
+          <input type="checkbox" name="join-alliance" id="join-alliance" aria-labelledby="join-alliance-label" v-model="alliance">
           <span id="join-alliance-label">{{ $t('signup.options.alliance') }}</span>
         </label>
       </div>
@@ -37,11 +37,39 @@
 </template>
 
 <script setup>
+import { watch } from 'vue';
+import { useSignupForm } from '../composables/useSignup';
+import { toast } from 'vue3-toastify';
+import 'vue3-toastify/dist/index.css';
+
 const { locale } = useI18n();
 
 const signup = ref((await queryContent('pages', 'signup').findOne()).signup[locale.value]);
 watch(locale, async () => {
   signup.value = (await queryContent('pages', 'signup').findOne()).signup[locale.value];
+});
+
+const {
+    email,
+    updates,
+    alliance,
+    conversation,
+    success,
+    onSubmit } = useSignupForm();
+
+const notify = () => {
+  toast("Sent successfully!", {
+    "theme": "auto",
+    "type": "success",
+    "autoClose": 600,
+    "position": "bottom-right"
+  })
+};
+
+watch(success, (value) => {
+  if (value) {
+    notify();
+  }
 });
 
 </script>

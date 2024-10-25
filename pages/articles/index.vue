7f93044d4531294771464ba98055b93adaaa7a14
Claudio <claudio@ccmdesign.ca>
2024-10-25 15:38:21 -0300

    search WIP

--- pages/articles/index.vue
+++ pages/articles/index.vue
@@ -2,6 +2,13 @@
   <div>
     <rd-hero :content="block_hero" />
     <rd-base-section color="base">
+      <div class="center">
+        <fieldset class="search-field">
+          <input type="search">
+          <input type="submit" class="button" data-size="s" data-visual="primary" value="Search">
+        </fieldset>
+        
+      </div>
       <cluster-l class="filters | padding-bottom:s1" justify="center">
         <label class="button" data-size="s"><input type="checkbox" id="">Guest Blog</label>
         <label class="button" data-size="s"><input type="checkbox" id="">New Publication</label>
@@ -29,6 +36,10 @@ const articles = await queryContent(locale.value, 'articles').find();
 </script>
 
 <style lang="scss" scoped>
+.center {
+  max-width: 600px;
+  width: 100%;   
+}
 
 .button {
   --button-color: hsla(var(--rd-purple), 1);
@@ -39,5 +50,23 @@ const articles = await queryContent(locale.value, 'articles').find();
 .button input[type="checkbox"] { display: none; }
 .button:has(input:checked) { background-color: var(--button-color); }
 
+.button[data-visual="primary"] { 
+  border-color: var(--button-color);
+  background-color: var(--button-color);
+  border-radius: 0 4px 4px 0;
+}
 
+
+.search-field {
+  all:unset;
+  display: flex;
+  width: 100%;
+}
+
+.search-field input[type="search"] {
+  flex: 1;
+  border: 2px solid hsla(var(--rd-purple), 1);
+  background-color: hsla(var(--white-hsl), .05);
+  border-radius: 4px 0 0 4px;
+}
 </style>

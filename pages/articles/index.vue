5ff8b955f87d430529ff584a9a790c6a52ca4681
Claudio <claudio@ccmdesign.ca>
2024-10-25 15:24:00 -0300

    filters for blog (interface only)

--- pages/articles/index.vue
+++ pages/articles/index.vue
@@ -2,6 +2,14 @@
   <div>
     <rd-hero :content="block_hero" />
     <rd-base-section color="base">
+      <cluster-l class="filters | padding-bottom:s1" justify="center">
+        <label class="button" data-size="s"><input type="checkbox" id="">Guest Blog</label>
+        <label class="button" data-size="s"><input type="checkbox" id="">New Publication</label>
+        <label class="button" data-size="s"><input type="checkbox" id="">New Video</label>
+        <label class="button" data-size="s"><input type="checkbox" id="">Event</label>
+        <label class="button" data-size="s"><input type="checkbox" id="">Announcement</label>
+        <label class="button" data-size="s"><input type="checkbox" id="">Explainer</label>
+      </cluster-l>
       <rd-card-grid>
         <rd-card v-for="post in articles" v-bind:key="post.slug" :content="post" source="blog"/>
       </rd-card-grid>
@@ -21,5 +29,14 @@ const articles = await queryContent(locale.value, 'articles').find();
 </script>
 
 <style lang="scss" scoped>
+
+.button {
+  --button-color: hsla(var(--rd-purple), 1);
+  --button-text-color: hsla(var(--rd-white), 1);
+}
+
+.button input[type="checkbox"] { display: none; }
+.button:has(input:checked) { background-color: var(--button-color); }
+
 
 </style>

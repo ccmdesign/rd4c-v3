aaebc11ebeacf8e96c9ab0c44156a2a36f272eef
Douglas Cantao <dcp.pereira@gmail.com>
2026-01-06 13:54:10 -0800

    Revert "Updating the compatibility date nuxt.config"
    This reverts commit ce47eac82bf02d3617d68ea87a5e23d37e219534.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -123,28 +123,5 @@ export default defineNuxtConfig({
     }
   },
 
-  compatibilityDate: '2024-04-03',
-
-  vite: {
-    plugins: [
-      {
-        enforce: 'pre',
-        config(config) {
-          const jsonPlugin = config.plugins.find(p => p && p.name === 'vite:json');
-          if (jsonPlugin) {
-            const orgTransform = jsonPlugin.transform;
-            // @ts-ignore
-            jsonPlugin.transform = function (code, id) {
-              // @ts-ignore
-              if (typeof orgTransform === 'function') {
-                // @ts-ignore
-                return orgTransform.apply(this, [code, id]);
-              }
-              return null;
-            };
-          }
-        },
-      },
-    ],
-  },
+  compatibilityDate: '2025-03-11',
 });
